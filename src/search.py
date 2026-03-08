d1099eb9a20c01fcaa85716a6969407e70d26cc2
Wagner Palmeira <wagnerpalmeira.tec@gmail.com>
2026-03-08 17:57:11 -0300

    Feat: adicionando busca por equivalent

--- src/search.py
+++ src/search.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from ingest import validate_env
+
+load_dotenv()
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -26,4 +34,18 @@ RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
 def search_prompt(question=None):
-    pass
+    validate_env()
+    embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL","text-embedding-3-small"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PGVECTOR_COLLECTION"),
+        connection=os.getenv("PGVECTOR_URL"),
+        use_jsonb=True,
+    )
+
+    results = store.similarity_search_with_score(question, k=10)
+
+    contexto = "\n\n".join(doc.page_content.strip() for doc, _ in results)
+    return PROMPT_TEMPLATE.format(contexto=contexto, pergunta=question)
+
